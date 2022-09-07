bd19b09fdbb1d3ce0ba7d6bd4e296ea56ec3a158
thisisommore <ommore501@gmail.com>
2022-09-07 18:18:53 +0000

    Fix modal closing on background click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,12 +5,14 @@ const FormModal: React.FC<React.PropsWithChildren> = (p) => {
   const [active, setActive] = useState(true);
 
   return (
-    <StyledModal
-      className={active ? "active" : ""}
-      onClick={() => setActive(false)}
-    >
-      <div className="modal bg-black p-20">{p.children}</div>
-    </StyledModal>
+    <>
+      <StyledModal className={active ? "active " : ""}>
+        <div className="modal z-10 fixed left-1/2 top-1/2 -translate-x-1/2 translate-y-1/2 bg-black p-20 w-5/6">
+          {p.children}
+        </div>
+        <div className="bg" onClick={() => setActive(false)} />
+      </StyledModal>
+    </>
   );
 };
 export default FormModal;
--- src/components/StylesModal.tsx
+++ src/components/StylesModal.tsx
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 
 const StyledModal = styled.div`
-  position: fixed;
-  display: flex;
-  top: 0;
-  left: 0;
-  backdrop-filter: blur(4px);
-  background-color: #ffffff3b;
-  height: 100vh;
-  width: 100vw;
-  transform: translate(-100%);
+  div.bg {
+    position: fixed;
+    display: flex;
+    top: 0;
+    left: 0;
+    backdrop-filter: blur(4px);
+    background-color: #ffffff3b;
+    z-index: 1;
+
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+  }
   transition: 0.3s;
-  justify-content: center;
-  align-items: center;
+
+  transform: translate(-100%);
+
   &.active {
     transform: translate(0);
   }
